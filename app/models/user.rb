0a6d8eb787ba2dff346e3016c1e87785f6134050
Joshua Park <joshuapark1309@gmail.com>
2025-07-01 01:07:36 -0400

    Twitter Account Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
 # password_confirmation:string virtual
 
 class User < ApplicationRecord
+  has_many :twitter_accounts
   has_secure_password
 
   validates :email, presence: true, format: { with: /[^@\s]+@[^@\s]+\.com\z/ }
